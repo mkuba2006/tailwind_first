bde0547ffae5528d892dd89d8280a21f3453e429
Jakub Myrta <mkuba2006@gmail.com>
2023-07-11 23:35:13 +0200

    first commit

---
+++ tail2/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      width: {
+        'maxx': '100vw',
+        'xl-container': '400px',
+        'xl-item': '300px',
+        'xl-img-container': '170px',
+        'xl-text': '210px',
+      },
+      height: {
+        'na100': '100%',
+        'xl-container': '200px',
+        'xl-img-container': '170px',
+      },
+      padding:{
+        'xl-img-container': '5px',
+      }
+    },
+  },
+  plugins: [],
+}
+
