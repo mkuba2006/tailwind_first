384d773e9f95ff468be2aeb292062f6170cf6460
Jakub Myrta <mkuba2006@gmail.com>
2023-07-13 23:42:34 +0200

    13.07

--- tail2/tailwind.config.js
+++ tail2/tailwind.config.js
@@ -1,9 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-  content: ["./dist/*.{html,js}"],
+  content: ["./dist/*.html"],
   theme: {
     screens: {
       sm: '480px',
@@ -18,27 +14,17 @@ module.exports = {
         'xl-item': '300px',
         'xl-img-container': '170px',
         'xl-text': '210px',
-        'xl-div': '90%',
-      },
-      maxWidth: {
-        'custom': '1200px',
       },
       height: {
         'na100': '100%',
         'xl-container': '200px',
         'xl-img-container': '170px',
       },
-      padding:{
+      padding: {
         'xl-img-container': '5px',
-      },
-      theme: {
-        fontFamily: {
-          sans: ['Graphik', 'sans-serif'],
-          serif: ['Merriweather', 'serif'],
-        },
       },
     },
   },
+  variants: {},
   plugins: [],
-}
-
+};
